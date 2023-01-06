ab7bf16f30ce77cbc21cd858d19cc210a5c173c7
newrank <80915048+newrank1@users.noreply.github.com>
2023-01-06 22:59:41 +0200

    Update text.js

--- assets/js/text.js
+++ assets/js/text.js
@@ -20,7 +20,7 @@
 });
 $(function() {
   $(".typed").typed({
-      strings: ["just a computer science student", "operating with linux and windows, also with Bash/Shell", "/ᐠ - ˕ - ﾏ", "thanks to Kaseki and Imadraude", "Don't be silent, stop the war", "Слава Україні"],
+      strings: ["r/archlinux", "but operating with Win as well", "PM is welcome", "if I don't answer you for a long time - I'm not interested", "/ᐠ - ˕ - ﾏ", "defragment your emotions"],
       typeSpeed: 100,
       loop: true,
       cursorChar: "|",
